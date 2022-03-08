e9f463c48f509f1eb9c328982272fb241a2706b2
Espen Gjendem <espen.gjendem@gmail.com>
2022-03-08 14:26:23 +0100

    done

--- src/components/CalendarBox.tsx
+++ src/components/CalendarBox.tsx
@@ -1,11 +1,19 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Icon, IconButton } from "@chakra-ui/react";
 import React, { useState } from "react";
 import Calendar from "./Calendar";
 import "./CalendarBox.css";
-import { SearchIcon } from "@chakra-ui/icons";
-import { addMonths, addDays, format, isAfter } from "date-fns";
+import { SearchIcon, StarIcon } from "@chakra-ui/icons";
+import {
+  addMonths,
+  addDays,
+  format,
+  isAfter,
+  differenceInDays,
+  subMonths,
+} from "date-fns";
 import nb from "date-fns/locale/nb";
 import Cell from "./Cell";
+import { isBetween } from "../helpers/functions";
 
 interface CalendarBoxProps {
   title?: string;
@@ -15,6 +23,23 @@ function CalendarBox({ title = "Velg dato" }: CalendarBoxProps) {
   let [currentDate, setDate] = useState(new Date());
   let [fromDate, setFrom] = useState(null);
   let [toDate, setTo] = useState(null);
+
+  function isCellBetweenBookings(cellDate: Date, bookings: any[]) {
+    return bookings.some((booking) =>
+      isBetween(cellDate, booking.from, booking.to)
+    );
+  }
+
+  let bookings = [
+    {
+      from: addDays(new Date(), 10),
+      to: addDays(new Date(), 15),
+    },
+    {
+      from: addDays(new Date(), 17),
+      to: addDays(new Date(), 22),
+    },
+  ];
 
   let onCellClick = (options: any) => {
     if (fromDate && toDate) {
@@ -27,6 +52,51 @@ function CalendarBox({ title = "Velg dato" }: CalendarBoxProps) {
     }
   };
 
+  function renderthis(options: any) {
+    if (options.isBetween) {
+      return (
+        <IconButton aria-label="" icon={<StarIcon />} key={options.index} />
+      );
+    }
+
+    if (options.cellDate == null) return <div key={options.index}></div>;
+
+    if (isCellBetweenBookings(options.cellDate, bookings)) {
+      return (
+        <Button key={options.index} colorScheme={"red"} m={1}>
+          {options ? options.cellDate?.getDate() : null}
+        </Button>
+      );
+    }
+
+    return (
+      <Button
+        onClick={() => onCellClick(options)}
+        key={options.index}
+        disabled={options.isPast}
+        colorScheme={
+          options.isBetween
+            ? "purple"
+            : options.isSelected
+            ? "pink"
+            : options.isPast
+            ? "gray"
+            : "teal"
+        }
+        m={1}
+      >
+        {options ? options.cellDate?.getDate() : null}
+      </Button>
+    );
+    /* <Cell
+        onClick={() => onCellClick(options)}
+        isSelected={options.isSelected}
+        key={options.index}
+      >
+        {options.cellDate ? options.cellDate.getDate() : null}
+      </Cell> */
+  }
+
   return (
     <div className="calendar-box">
       <div className="title">{title}</div>
@@ -34,38 +104,28 @@ function CalendarBox({ title = "Velg dato" }: CalendarBoxProps) {
       <div className="calendar1">
         <Calendar
           date={currentDate}
+          onNext={(newDate) => setDate(newDate)}
           onPrev={(newDate) => setDate(newDate)}
           fromDate={fromDate}
           toDate={toDate}
-          renderCell={(options) => (
-            <Cell
-              onClick={() => onCellClick(options)}
-              isSelected={options.isSelected}
-              key={options.index}
-            >
-              {options.cellDate ? options.cellDate.getDate() : null}
-            </Cell>
-          )}
+          renderCell={renderthis}
         />
       </div>
       <div className="calendar2">
         <Calendar
           date={addMonths(currentDate, 1)}
           onNext={(newDate) => setDate(newDate)}
+          onPrev={(newDate) => setDate(subMonths(newDate, 1))}
           fromDate={fromDate}
           toDate={toDate}
-          renderCell={(options) => (
-            <Cell
-              onClick={() => onCellClick(options)}
-              isSelected={options.isSelected}
-              key={options.index}
-            >
-              {options.cellDate ? options.cellDate.getDate() : null}
-            </Cell>
-          )}
+          renderCell={renderthis}
         />
       </div>
-      <div className="rules"></div>
+      <div className="rules">
+        {fromDate != null &&
+          toDate != null &&
+          `Days: ${differenceInDays(toDate, fromDate)}`}
+      </div>
       <div className="buttons">
         <Button leftIcon={<SearchIcon />}>Book</Button>
       </div>
